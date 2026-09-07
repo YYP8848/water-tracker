ad9e2c367627d2ca771e01a40e6ebcb2fc3f9ee4
YYP8848 <YYP8848@users.noreply.github.com>
2026-09-07 18:04:08 +0800

    冻结历史喝水记录 + 支持随意调节毫升

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'water-tracker-v1';
+const CACHE = 'water-tracker-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
 
 self.addEventListener('install', (e) => {
